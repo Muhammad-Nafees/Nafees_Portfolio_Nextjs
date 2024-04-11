e2bcdc0b1241985b1d5ca482d9ef1fb26d330202
Nafees <nf10018@gmail.com>
2024-04-11 21:37:10 +0500

    experienced-section-working

--- Components/HeaderTop.jsx
+++ Components/HeaderTop.jsx
@@ -13,7 +13,7 @@ const HeaderTop = () => {
 
     return (
 
-        <div className="px-10 justify-center  bg-nafees flex pt-5">
+        <div className="px-10 justify-center flex pt-5">
             <div className="header_top">
                 <Link href="/">
                     <h3 className="text-white font-bold">Muhammad Nafees</h3>
---
+++ Components/reusable_components/SkillsMention.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { FiCheck } from 'react-icons/fi'
+
+const SkillsMention = ({ skill, experience }) => {
+    return (
+        <div className='py-4 flex items-center'>
+            <div className='mx-5 w-[20px] flex items-center justify-center h-[20px] rounded-full bg-black'>
+                <FiCheck size={14} color='white' />
+            </div>
+            <div>
+                <h3 className='text-md font-bold'>{skill}</h3>
+                <p className='text-sm'>{experience}</p>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default SkillsMention;
