f25d594f52b152414209e0b0774b6d0617522d0f
Muhammad Nafees <nf10018@gmail.com>
2024-04-22 02:57:09 +0500

    formik uses

--- Components/contact/MainContentContact.jsx
+++ Components/contact/MainContentContact.jsx
@@ -1,45 +1,78 @@
+"use client"
 import { contactForm } from "@/validation/validation";
 import CustomInput from "../reusable_components/CustomInput";
 import { Formik } from "formik";
 
-
 const MainContentContact = () => {
 
-  const initialValues={
-     name:"",
-     email:"",
-     textArea:""
+  const initialValues = {
+    name: "",
+    email: "",
+    textArea: "",
+  };
+
+  const handleSubmit =async(values)=>{
+      console.log("valuesName",values);
+      console.log("OK");
   }
 
   return (
     <>
-    <Formik
-      initialValues={initialValues}
+      <Formik initialValues={initialValues}
        validationSchema={contactForm}
-     >
-      <div className="pt-20 flex flex-col justify-center items-center">
-        <p className="text-gray-500 text-sm py-2">Get In Touch</p>
-        <h1 className="text-white font-bold text-4xl">Contact Me</h1>
-      </div>
+       onSubmit={handleSubmit}
+       >
+        {({ errors, values, handleChange, touched ,handleBlur}) => (
+          
+          <>
+            <div className="pt-20 flex flex-col justify-center items-center">
+              <p className="text-gray-500 text-sm py-2">Get In Touch</p>
+              <h1 className="text-white font-bold text-4xl">Contact Me</h1>
+            </div>
 
-      <div className="py-12">
-        <CustomInput placeHolder={"Name"} />
-        <CustomInput placeHolder={"Email"} />
 
-        <div className="w-5/12 border border-gray-500 py-2 rounded-lg m-auto my-4">
-          <textarea
-            placeholder={"Type Your Message"}
-            className="pl-3 h-32 bg-transparent border-none outline-none w-11/12 resize-none"
-          />
-        </div>
+            <div className="py-12">
+               <form onSubmit={handleSubmit}>
 
-         <button type="submit" className="px-8 py-3 bg-nafeesOne flex justify-center items-center m-auto rounded-lg">
-             <p className="text-black font-semibold">Send</p>
-         </button>
+              <CustomInput 
+               
+               placeHolder={"Name"} 
+               value={values.name}
+               error={errors.name}
+               onChangeInputValue={handleChange("name")}
+               onBlur={handleBlur}
+               />
 
-      </div>
-    </Formik>
+              <CustomInput
+               placeHolder={"Email"}
+               value={values.email}
+               error={errors.email}
+               onChangeInputValue={handleChange("email")}
+               onBlur={handleBlur}
+               />
 
+              <div className="w-5/12 border border-gray-500 py-2 rounded-lg m-auto my-4">
+                <textarea
+                  value={values.textArea}
+                  onChange={handleChange("textArea")}
+                  onBlur={handleBlur}
+                  placeholder={"Type Your Message"}
+                  className="pl-3 h-32 bg-transparent border-none outline-none w-11/12 resize-none"
+                  />
+              </div>
+            </form>
+
+              <button
+                onClick={handleSubmit}
+                type="submit"
+                className="px-8 py-3 bg-nafeesOne flex justify-center items-center m-auto rounded-lg"
+              >
+                <p className="text-black font-semibold">Send</p>
+              </button>
+            </div>
+          </>
+        )}
+      </Formik>
     </>
   );
 };
--- Components/reusable_components/CustomInput.jsx
+++ Components/reusable_components/CustomInput.jsx
@@ -1,13 +1,21 @@
-
-const CustomInput =({placeHolder}) =>{
-return(
-    <div className="w-5/12 border border-gray-500 py-2 rounded-lg m-auto my-4">
+const CustomInput = ({ placeHolder, onChangeInputValue, value, error,onBlur }) => {
+  return (
+    <>
+      <div className="w-5/12 m-auto h-18">
+        <div className="border border-gray-500 py-2 rounded-lg my-4">
           <input
+            value={value}
+            autoComplete="off"
+            onChange={(e) => onChangeInputValue(e.target.value)}
             placeholder={placeHolder}
+            onBlur={onBlur}
             className="pl-3 bg-transparent border-none outline-none w-11/12"
           />
         </div>
-)
-}
+        <p className="text-red-600">{error}</p>
+      </div>
+    </>
+  );
+};
 
 export default CustomInput;
