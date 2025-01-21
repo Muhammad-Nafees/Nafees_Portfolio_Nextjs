90c50ae567046f7ce0620fd6a431ad4d772c0048
Muhammad Nafees <nf10018@gmail.com>
2025-01-22 01:42:11 +0500

    resolve-text

--- Components/about/MainContentAbout.jsx
+++ Components/about/MainContentAbout.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const MainContentAbout = () => {
   return (
-    <div className="py-20 w-full mx-auto lg:flex items-center lg:w-[70%]">
+    <div className="py-20 w-full mx-auto lg:flex items-center lg:w-[70%] ">
       <div
         whileHover={{ scale: 1.05 }}
         className="hover:translate-y-1 transition duration-300 ease-in-out w-4/5 h-4/5 sm:w-2/5 m-auto flex items-center justify-center"
@@ -17,9 +17,8 @@ const MainContentAbout = () => {
         />
       </div>
 
-      <div className="lg:w-[70%] flex flex-col px-10 ">
+      <div className="lg:w-[70%] flex flex-col px-10">
         <div className="py-12 flex flex-col md:flex-row items-center justify-center lg:justify-start lg:items-start space-y-4 md:space-y-0 md:space-x-4 ">
-
           <div className="hover:-translate-y-0.5 transition duration-300 ease-in-out border px-10 h-40 rounded-3xl flex flex-col justify-center items-center">
             <Image
               width={25}
@@ -42,14 +41,11 @@ const MainContentAbout = () => {
               src={require("../../public/education.png")}
             />
             <p className="text-black font-bold">Education</p>
-            <p className="text-center text-sm sm:text-base">
-              Graduation Bscs
-            </p>
+            <p className="text-center text-sm sm:text-base">Graduation Bscs</p>
           </div>
-
         </div>
 
-        <div className="w-[100%] sm:w-[80%] sm:mx-auto lg:mx-0 flex items-center justify-center lg:justify-start lg:items-start sm:block">
+        <div className="w-[100%] sm:w-[100%] sm:mx-auto lg:mx-0 flex items-center justify-center lg:justify-start lg:items-start sm:block">
           <p className="text-black font-medium text-sm lg:text-sm xl:text-base">
             I am a passionate React Native developer with over a year of
             experience in the field. I have successfully designed and developed
@@ -60,7 +56,6 @@ const MainContentAbout = () => {
             high-quality results in every project I undertake
           </p>
         </div>
-
       </div>
     </div>
   );
